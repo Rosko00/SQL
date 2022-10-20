39888121bed17f2ce0d7d2665f8bce2ce58ce028
Marián Gálik <marian.galik@gmail.com>
2022-10-20 11:09:07 +0200

    Update import_data_csv.py

--- import_data_csv.py
+++ import_data_csv.py
@@ -1,3 +1,13 @@
+import mysql.connector
 import pandas as pd # import library 
+# work from databases
 data_conti = pd.read_csv('172_17_0_1.csv') # import data
 print (data_conti) # print data
+
+# connection localhost
+my_data = mysql.connector.connect (
+    host = "/tmp/mysql80.sock",  # sql address on the server
+    user = "yourusername", # often root or admin
+    password = "yourpassword"  
+)
+print(my_data)
